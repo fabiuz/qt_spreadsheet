5574ff1ba9fb5cb50a3d2e888ac1fe536a5154cb
fabiuz <ccmaismais@yahoo.com>
2017-10-26 17:24:34 -0200

    Pequenas correções em nome de variáveis.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char *argv[]){
     QSplashScreen *splash = new QSplashScreen;
     splash->setPixmap(QPixmap(":/images/splash.png"));
     splash->show();
-    Qt::Alignment topRigth = Qt::AlignRight | Qt::AlignTop;
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
     splash->showMessage(QObject::tr("Setting up the main window..."),
-                        topRigh, Qt::white);
+                        topRight, Qt::white);
     MainWindow mainWin;
     splash->showMessage(QObject::tr("Loading modules..."), topRight, Qt::white);
     loadModules();
